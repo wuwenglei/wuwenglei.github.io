c8343e1df0965d49b8323dba0d706669f2592c9f
Wenglei Wu <wwu077@uottawa.ca>
2021-05-29 18:19:26 +0800

    add arrow before the selected items in the shopping cart

--- lab2/scripts/main.js
+++ lab2/scripts/main.js
@@ -86,7 +86,7 @@ function selectedItems(){
 	para.appendChild(document.createElement("br"));
 	for (i = 0; i < ele.length; i++) {
 		if (ele[i].checked) {
-			para.appendChild(document.createTextNode(ele[i].value));
+			para.appendChild(document.createTextNode("-> "+ele[i].value));
 			para.appendChild(document.createElement("br"));
 			chosenProducts.push(ele[i].value);
 		}
